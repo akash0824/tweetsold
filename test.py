70c88ea48564eef63172f1225abfc46d33a93bc0
AKASH KUAMR <akashkumar0824@gmail.com>
2017-12-09 23:24:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import tweetsold as to
-
+import json
 
 consumer_key = 'COETny0iHmAF2myVrHaszW9f6'
 consumer_secret = 'PCj3wkUNUXgd5bkBEuPKjYwCjHkQASWzZZCnrIHIs4Vbg6zlkz'
@@ -8,10 +8,11 @@ access_secret = 'cAkr6oy6PUQyObH9jP5ax2jtPAIwEsOHHqMJA6rtdxKaB'
 
 
 client = to.authentication(consumer_key, consumer_secret, access_token, access_secret)
-tweets = to.get_tweets(client, ["python","java"])
 
+tweets = to.get_tweets(client, ["ruby", "javascript"], "2017-12-03")
 
-print(tweets[0])
+with open("test.json","w") as file:
+    json.dump(tweets, file)
 
 
 
