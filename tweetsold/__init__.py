49881b7f38862c6377e5a5f3e110b868d2af4067
AKASH KUAMR <akashkumar0824@gmail.com>
2017-12-09 23:22:12 +0530

    Update __init__.py

--- tweetsold/__init__.py
+++ tweetsold/__init__.py
@@ -1,21 +1,26 @@
 import oauth2
 import json
+import time
 
 
 def authentication(consumer_key, consumer_secret, access_token, access_secret):
     consumer = oauth2.Consumer(key = consumer_key, secret = consumer_secret)
     token = oauth2.Token(key = access_token, secret = access_secret)
     client = oauth2.Client(consumer, token)
-
     return client
 
-def get_tweets(client, qry):
+def get_tweets(client, qry, until = ""):
+    print("Process : Extrction of tweets is running...This will take sometime...")
     qry = " OR ".join(qry)
-    url = "https://api.twitter.com/1.1/search/tweets.json?count=100&q=" + qry
+    if until != "":
+        url = "https://api.twitter.com/1.1/search/tweets.json?until=" + until + "&count=100&q=" + qry
+    else:
+        url = "https://api.twitter.com/1.1/search/tweets.json?count=100&q=" + qry
     
     fulldata = []
     while True:
     	try:
+    	    #print("step1")
             resp, content = client.request( url, method="GET", body=b"", headers=None )
             content = json.loads(content)
             #print(content)
@@ -28,12 +33,19 @@ def get_tweets(client, qry):
 
             max_id = str(min(temp)-1)
 
-            url = "https://api.twitter.com/1.1/search/tweets.json?count=100&max_id=" + max_id + "&q=" + qry
-            print(url)
+            if until != "": 
+                url = "https://api.twitter.com/1.1/search/tweets.json?until=" + until + "&count=100&max_id=" + max_id + "&q=" + qry
+            else:
+                url = "https://api.twitter.com/1.1/search/tweets.json?count=100&max_id=" + max_id + "&q=" + qry
+            
+            print("Extracted " + str(len(fulldata)) + " tweets till now.")
+            time.sleep(5)
         except:
-            print("break")
+            #print("break")
             break
-
+    print("\n-------------------------------------------------------------")
+    print("Total " + str(len(fulldata)) + " Extracted.")
+    print("-------------------------------------------------------------")
     return fulldata
 
 
